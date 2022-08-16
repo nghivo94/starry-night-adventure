9a24f8f6b8384a3027b62541eacc75340332264f
Dong Nghi Vo <nghi.vodong942003@gmail.com>
2022-08-16 16:59:22 +0700

    effects.js commented.

--- scripts/effects.js
+++ scripts/effects.js
@@ -1,7 +1,22 @@
-export class Effect {
+//Initialization: a fixed array for all possible effect types (for management)
+const EFFECT_TYPES = ["end", "talk", ""];
+Object.freeze(EFFECT_TYPES);
+
+//Class Effect
+class Effect {
     constructor (type, target) {
         this.type = type;
         this.target = target;
-        Object.freeze(this);
+        Object.freeze(this); //Make the object immutable
+    }
+
+    //Creation method to also check type
+    static create (type, target) {
+        if (EFFECT_TYPES.includes(type)) {
+            return new Effect(type, target);
+        }
+        else return undefined;
     }
 }
+
+export { Effect }
