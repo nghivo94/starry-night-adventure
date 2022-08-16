634b86668ce8bd13e8858c337bc3ad74b4aaf0ab
Dong Nghi Vo <nghi.vodong942003@gmail.com>
2022-08-16 22:38:40 +0700

    Background music added.

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,29 @@
 import { world } from "./world.js";
 
-console.log(world.getView());
+const descriptionPane = document.querySelector("#description-pane");
+const logPane = document.querySelector("#log-pane");
+document.querySelector("#background-music").volume = 0;
+
+function init () {
+    const initResult = world.init();
+    renderChapter(initResult.chapter);
+    logPane.textContent = initResult.line;
+
+}
+
+function renderChapter (chapter) {
+    document.querySelector(".chapter h1").textContent = chapter.chapter;
+    document.querySelector(".chapter h2").textContent = chapter.title;
+    
+    const chapterPane = document.querySelector(".chapter");
+    chapterPane.style.zIndex = 1;
+    chapterPane.style.opacity = 1;
+    setTimeout(() => {
+        chapterPane.style.opacity = 0;
+        setTimeout(() => {
+            chapterPane.style.zIndex = -1;
+        }, 600)
+    },1800);
+}
+
+init();
