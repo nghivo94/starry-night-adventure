d38543f94cea69c1c2336d4d40e95f3221cd3e51
Dong Nghi Vo <nghi.vodong942003@gmail.com>
2022-08-17 01:44:16 +0700

    Kick start first scene!!!

--- scripts/characters.js
+++ scripts/characters.js
@@ -112,7 +112,37 @@ class Character {
 
 Character.create ("", [
     new Dialog(
-        "Tryingg so hard man", 
+        `
+        <p class="img-detail">Orion constellation - The Hunter</p>
+        <img class="center-portrait-img" src="https://drive.google.com/uc?export=view&id=16cDtMtYwnimZuefOkPG6su5DaAmCGUVd">
+        <p class="story-telling">"This is Orion constellation, named after a hunter in Greek Mythology. 
+            Legend has it that Orion was a supernaturally powerful hunter, born to the God of Seas Poseidon. 
+            Yet as he enraged Gaea, Mother of Earch, she sent a scorpio to dispatch him. 
+            This is how the ancient Greek explained why Opion and Scorpius constellations never appear at the same time."</p>
+        <p>"Oh, seems like life in the sky is not as easy either."</p>
+        <p>Someone in the crowd commented at the descriptions of the museum guide, followed by frequent chatters and giggles.</p>
+        <div class="block text-center">
+            <p class="status-description">You are on a museum trip, in the astrology section.</p>
+            <p class="status-description">Little did you know, you were signing up for something much more.</p>
+        </div>
+        <p>As you are roaming around paintings of constellations, a tall man bumps into you. 
+            His face is entirely covered with a hood, sunglasses and a mask. 
+            He quickly mumbles an apology and disappear amidst the stream of people towards the exit.</p>
+        <div class="float-right-container">
+            <p>
+                Yet you notice something the man left behind, right beneath your feet.
+                It is a chess piece. An exquisite one to be precise. 
+                It is made of marble, glistening in the light. 
+                The workmanship is immaculate, shaping the black surface into a beautiful pawn piece.
+            </p>
+            <img class="float-right-img" src="https://drive.google.com/uc?export=view&id=1uG4H6P9WH-Blj6iL87NKYr8sBcpdDDTh">
+        </div>
+        <p>Choices:</p>
+        <p class="note">Note: for first time players, it is recommended to choose the first option.</p>
+        <label><input type="radio" name="dialog-option">Pick up the chess piece</label>
+        <label><input type="radio" name="dialog-option">Leave the chess piece</label>
+        <button id="choose">Choose</button>
+        `, 
         {
             "1": new DialogOption(0, [Effect.create("end", "nonstart-end")], "You ignored the dropped object and proceeded as usual."),
             "2": new DialogOption(1, [Effect.create("talk", "shopkeeper")], "You picked up the strange object.")
